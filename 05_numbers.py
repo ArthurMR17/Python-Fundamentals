ab72c22da9d74854e3a245855d2c37ac441fe93f
Arturo Merlano <arturomerlano17@hotmail.com>
2022-12-28 15:07:52 -0500

    clase 10, numeros enteros y float

---
+++ 05_numbers.py
@@ -0,0 +1,21 @@
+# numeros enteros
+lives = 3
+print(type(lives))
+age =12
+budget = 100
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+lives -= 1
+print(lives)
+
+
+
+# numeros coma flotante
+temperatura = 12.12
+numnber = 450000000000000.1
+print(numnber)
+number2 = 0.000000000001
+print(number2)
